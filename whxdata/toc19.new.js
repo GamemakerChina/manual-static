45dd0f544ea35e298b89174a59c787fa448b987c
LiarOnce <qq1062787843@hotmail.com>
2023-07-31 09:13:59 +0000

    Deploying to main from @ GamemakerChina/GameMaker-maunl-zh@dae71e74da806456cf4b20eee55e013996b8dbca 🚀

--- whxdata/toc19.new.js
+++ whxdata/toc19.new.js
@@ -1,6 +1,6 @@
 
                 (function() {
-                    var toc = [{"name":"语法错误(羽毛信息)","type":"item","tilePath":"","description":"Page that explains the various syntax errors","url":"Additional_Information/Errors/Syntax_Errors.htm"},{"name":"编译器错误","type":"item","tilePath":"","description":"Page that explains the various compiler errors","url":"Additional_Information/Errors/Compiler_Errors.htm"},{"name":"运行器错误","type":"item","tilePath":"","description":"Page that explains the various runner errors","url":"Additional_Information/Errors/Runner_Errors.htm"}];
+                    var toc = [{"name":"语法错误(语法检查信息)","type":"item","tilePath":"","description":"Page that explains the various syntax errors","url":"Additional_Information/Errors/Syntax_Errors.htm"},{"name":"编译器错误","type":"item","tilePath":"","description":"Page that explains the various compiler errors","url":"Additional_Information/Errors/Compiler_Errors.htm"},{"name":"运行器错误","type":"item","tilePath":"","description":"Page that explains the various runner errors","url":"Additional_Information/Errors/Runner_Errors.htm"}];
                     window.rh.model.publish(rh.consts('KEY_TEMP_DATA'), toc, {
                         sync: true
                     });

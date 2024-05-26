e405ff7518b77f43c488c24cc174002354bc0de1
LiarOnce <qq1062787843@hotmail.com>
2024-05-26 13:30:40 +0000

    Deploying to main from @ GamemakerChina/GameMaker-maunl-zh@d41524c2e27d2472a4aeb41718ef05e8a72e6f6c 🚀

--- whxdata/toc26.new.js
+++ whxdata/toc26.new.js
@@ -1,6 +1,6 @@
 
                 (function() {
-                    var toc = [{"name":"壁纸配置事件","type":"item","tilePath":"","description":"Reference page for the Wallpaper Config event, used with Live Wallpapers","url":"The_Asset_Editors/Object_Properties/Wallpaper_Config_Event.htm"}];
+                    var toc = [{"name":"壁纸事件","type":"item","tilePath":"","description":"Reference page for the Wallpaper Config event, used with Live Wallpapers","url":"The_Asset_Editors/Object_Properties/Wallpaper_Config_Event.htm"}];
                     window.rh.model.publish(rh.consts('KEY_TEMP_DATA'), toc, {
                         sync: true
                     });

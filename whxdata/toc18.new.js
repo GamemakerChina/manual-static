ac6bdba7a1f1076bb7f28c2ba4e55a10f2bbc465
LiarOnce <qq1062787843@hotmail.com>
2024-05-30 09:16:17 +0000

    Deploying to main from @ GamemakerChina/GameMaker-maunl-zh@423bf8962f603e4869ffe06dbdaff88b766f5df5 🚀

--- whxdata/toc18.new.js
+++ whxdata/toc18.new.js
@@ -1,6 +1,6 @@
 
                 (function() {
-                    var toc = [{"name":"纹理页","type":"item","tilePath":"","description":"Page that explains how texture pages work","url":"Settings/Texture_Information/Texture_Pages.htm"},{"name":"Texture Compression","type":"item","tilePath":"","description":"Overview page for texture compression","url":"Settings/Texture_Information/Texture_Compression.htm"},{"name":"动态纹理","type":"item","tilePath":"","description":"Dynamic Texture Groups and Pages","url":"Settings/Texture_Information/Dynamic_Textures.htm"}];
+                    var toc = [{"name":"纹理页","type":"item","tilePath":"","description":"Page that explains how texture pages work","url":"Settings/Texture_Information/Texture_Pages.htm"},{"name":"纹理压缩","type":"item","tilePath":"","description":"Overview page for texture compression","url":"Settings/Texture_Information/Texture_Compression.htm"},{"name":"动态纹理","type":"item","tilePath":"","description":"Dynamic Texture Groups and Pages","url":"Settings/Texture_Information/Dynamic_Textures.htm"}];
                     window.rh.model.publish(rh.consts('KEY_TEMP_DATA'), toc, {
                         sync: true
                     });
